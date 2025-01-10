2c63ccff20b75de1e448748fa334ba7d95e24659
Scrappy <mikelerogeratro@yahoo.it>
2025-01-10 15:53:58 +0100

    Fixed online PlayerSpawn

--- castle_crash/code/Game/PlayerSpawnManager.cs
+++ castle_crash/code/Game/PlayerSpawnManager.cs
@@ -1,6 +1,7 @@
 using Sandbox;
 using CastleCrash;
 using Sandbox.Diagnostics;
+using System;
 
 /*
 This works similar to NetworkHelper
@@ -11,31 +12,50 @@ public sealed class PlayerSpawnManagerComponent : Component, Component.INetworkL
 	[Property, Group( "Prefabs" )] public GameObject PlayerCellsPrefab { get; set; }
 	[Property] public GameObject[] SpawnPoints { get; set; }
 
+	Connection[] ConnectionToSpawnIndexArray;
+
+	protected override void OnAwake()
+	{
+		ConnectionToSpawnIndexArray = new Connection[SpawnPoints.Length];
+	}
+
 	// This is called on the host!
 	public void OnConnected( Connection channel )
 	{
-		Log.Info( $"Player '{channel.DisplayName}' connected" );
+		Log.Info( $"[PlayerManager]Player '{channel.DisplayName}' connected" );
 	}
 
 	// This is called on the host!
 	public void OnDisconnected( Connection channel )
 	{
-		Log.Info( $"Player '{channel.DisplayName}' disconnected" );
+		Log.Info( $"[PlayerManager]Player '{channel.DisplayName}' disconnected" );
+
+		int FoundIndex = Array.IndexOf( ConnectionToSpawnIndexArray, channel );
+		if ( FoundIndex >= 0 && FoundIndex < ConnectionToSpawnIndexArray.Length )
+		{
+			ConnectionToSpawnIndexArray[FoundIndex] = null;
+			Log.Info( $"[PlayerManager]Player '{channel.DisplayName}' Spawn Index {FoundIndex} removed" );
+		}
+		else
+		{
+			Log.Error( $"[PlayerManager]Unable to remove Player '{channel.DisplayName}' entry from the array, Index:{FoundIndex}" );
+		}
 	}
 
 	// This is called on the host!
 	public void OnActive( Connection channel )
 	{
-		Log.Info( $"Player '{channel.DisplayName}' has joined the game" );
+		Log.Info( $"[PlayerManager]Player '{channel.DisplayName}' has joined the game" );
 
 		SetupConnectionPermissions( channel );
 
-		var ConnectionList = Connection.All.ToList();
-		int SpawnIndexToUse = ConnectionList.IndexOf( channel );
-
+		// Find the first free slot (null)
+		int SpawnIndexToUse = Array.IndexOf( ConnectionToSpawnIndexArray, null );
+		Assert.True( SpawnIndexToUse != -1 );
 		Assert.True( SpawnIndexToUse <= SpawnPoints.Length );
+		ConnectionToSpawnIndexArray[SpawnIndexToUse] = channel;
 
-		Log.Info( $"Player '{channel.DisplayName}' is spawning at index {SpawnIndexToUse}-{SpawnPoints[SpawnIndexToUse]}" );
+		Log.Info( $"[PlayerManager]Player '{channel.DisplayName}' is spawning at index {SpawnIndexToUse}-{SpawnPoints[SpawnIndexToUse]}" );
 
 		var NewPlayerCells = PlayerCellsPrefab.Clone( SpawnPoints[SpawnIndexToUse].WorldTransform, name: $"Player - {channel.DisplayName}" );
 		NewPlayerCells.BreakFromPrefab();
@@ -45,6 +65,6 @@ public sealed class PlayerSpawnManagerComponent : Component, Component.INetworkL
 	void SetupConnectionPermissions( Connection channel )
 	{
 		channel.CanSpawnObjects = true;
-		channel.CanRefreshObjects = true;
+		channel.CanRefreshObjects = true; // Could not be needed
 	}
 }
