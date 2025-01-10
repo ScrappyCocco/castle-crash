ca457c06481b1ab20bd2b31f85c99da9ae84aece
Scrappy <mikelerogeratro@yahoo.it>
2025-01-10 11:33:42 +0100

    Fixed comments and added logs on PlayerSpawnManager

--- castle_crash/code/Game/PlayerSpawnManager.cs
+++ castle_crash/code/Game/PlayerSpawnManager.cs
@@ -2,26 +2,43 @@ using Sandbox;
 using CastleCrash;
 using Sandbox.Diagnostics;
 
+/*
+This works similar to NetworkHelper
+But it picks the SpawnPoints based on the player index in Connection List instead of Randomly picking it
+*/
 public sealed class PlayerSpawnManagerComponent : Component, Component.INetworkListener
 {
 	[Property, Group( "Prefabs" )] public GameObject PlayerCellsPrefab { get; set; }
 	[Property] public GameObject[] SpawnPoints { get; set; }
 
+	// This is called on the host!
+	public void OnConnected( Connection channel )
+	{
+		Log.Info( $"Player '{channel.DisplayName}' connected" );
+	}
+
+	// This is called on the host!
+	public void OnDisconnected( Connection channel )
+	{
+		Log.Info( $"Player '{channel.DisplayName}' disconnected" );
+	}
+
+	// This is called on the host!
 	public void OnActive( Connection channel )
 	{
+		Log.Info( $"Player '{channel.DisplayName}' has joined the game" );
+
 		SetupConnectionPermissions( channel );
 
-		// Is this stable?
-		// The better alternative would be to have a stable player-id as 0-1-2-...
-		// Otherwise we could do Client -Rpc-> Ask Host Spawn -Rpc-> Spawn there
 		var ConnectionList = Connection.All.ToList();
 		int SpawnIndexToUse = ConnectionList.IndexOf( channel );
 
 		Assert.True( SpawnIndexToUse <= SpawnPoints.Length );
 
+		Log.Info( $"Player '{channel.DisplayName}' is spawning at index {SpawnIndexToUse}-{SpawnPoints[SpawnIndexToUse]}" );
+
 		var NewPlayerCells = PlayerCellsPrefab.Clone( SpawnPoints[SpawnIndexToUse].WorldTransform, name: $"Player - {channel.DisplayName}" );
 		NewPlayerCells.BreakFromPrefab();
-		//NewPlayerCells.WorldRotation = SpawnPoints[SpawnIndexToUse].WorldRotation;
 		NewPlayerCells.NetworkSpawn( channel );
 	}
 
